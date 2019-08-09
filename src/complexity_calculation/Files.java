ab07891e1e034c795002dc3937c03471da911574
Malaka Silva <malakasilva43@gmail.com>
2019-08-09 13:52:23 +0530

    Changed Files.java

--- src/complexity_calculation/Files.java
+++ src/complexity_calculation/Files.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,26 +18,45 @@ import java.util.logging.Logger;
  * @author blitz
  */
 public class Files {
-    private static String filePath;
 
-    public static String getFilePath() {
-        return filePath;
-    }
+   private static String filePath;
+   private static BufferedReader buffR = null;
 
-    public static void setFilePath(String filePath) {
-        Files.filePath = filePath;
-    }
+   public static String getFilePath() {
+      return filePath;
+   }
 
-    public static BufferedReader loadFile(){
-        File file = new File(filePath);
-        BufferedReader buffR = null;
-        
-        try {
-            buffR = new BufferedReader(new FileReader(file));
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Files.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return buffR;
-    }
+   public static void setFilePath(String filePath) {
+      Files.filePath = filePath;
+   }
+
+   public static BufferedReader loadFile() {
+      File file = new File(filePath);
+
+      try {
+         buffR = new BufferedReader(new FileReader(file));
+      } catch (FileNotFoundException ex) {
+         Logger.getLogger(Files.class.getName()).log(Level.SEVERE, null, ex);
+      }
+
+      return buffR;
+   }
+
+   public static int getNumberOfLines() {
+      int count = 0;
+
+      if (buffR == null) {
+         return 0;
+      } else {
+         try {
+            while (buffR.readLine() != null) {
+               count++;
+            }
+         } catch (IOException ex) {
+            Logger.getLogger(Complexity_calculation.class.getName()).log(Level.SEVERE, null, ex);
+         }
+      }
+
+      return count;
+   }
 }
